1710efc1b9ade7dd6fb497b3a31b7bb931c752c2
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-08-20 13:34:52 +0200

    adds cli parameter

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"github.com/droundy/goopt"
 	"log"
 	"os"
 )
+
+var optVerbose = goopt.Flag([]string{
+	"-v", "--verbose"},
+	[]string{"--quiet"}, "output verbosely",
+	"be quiet, instead")
+
+var optType = goopt.Alternatives([]string{"--type"}, []string{"help", "parent"}, "type of upgrade")
 
 func init() {
 	log.SetOutput(os.Stdout)
@@ -11,5 +20,18 @@ func init() {
 }
 
 func main() {
+	parseParameter()
+
 	log.Print("test")
+}
+
+func parseParameter() {
+	goopt.Summary = "automatic upgrade maven projects"
+	goopt.Version = "0.1"
+	goopt.Parse(nil)
+
+	if *optType == "help" {
+		fmt.Print(goopt.Usage())
+		os.Exit(0)
+	}
 }
