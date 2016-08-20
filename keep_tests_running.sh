ee22816bb05fcf86539ab2c915a0e1a79cba7fb8
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-08-20 13:33:08 +0200

    changed to self restarting

--- keep_tests_running.sh
+++ keep_tests_running.sh
@@ -3,18 +3,21 @@
 set -e
 #set -x
 
-while true; do
-	inotifywait -r -e close_write,move_self *.sh *.go lib;
+inotifywait -r -e close_write,move_self *.sh *.go lib;
 
-    echo
-    echo " ---- RUN ---- "
-    echo
+echo
+echo " ---- RUN ---- "
+echo
 
-	set +e
-	echo running build
-	./build.sh || echo "ERROR"
+set +e
+echo running build
+./build.sh || echo "ERROR"
 
-	echo "executing"
-	./bin/upgrade || echo "ERROR"
-	set -e
-done
+echo "executing"
+set -x
+./bin/upgrade || echo "ERROR"
+set +x
+set -e
+
+# self restarting
+exec $0
