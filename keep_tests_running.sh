eb2af9ae8e2cfe5a997c02616c552ef73b928dcf
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-08-20 14:39:50 +0200

    cleanup

--- keep_tests_running.sh
+++ keep_tests_running.sh
@@ -3,13 +3,24 @@
 set -e
 #set -x
 
+pushd () {
+    command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null 2>&1
+}
+
 inotifywait -r -e close_write,move_self *.sh *.go lib;
 
 echo
 echo " ---- RUN ---- "
 echo
 
-./build.sh && ./bin/upgrade
+mkdir -p test
+./build.sh && pushd test && ../bin/upgrade --type=parent
+
+popd
 
 # self restarting
 exec $0
