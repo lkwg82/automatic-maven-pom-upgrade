2dc4929fad57d115eaa392cbb488fa5ea6bde813
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-08-28 20:50:20 +0200

    removes old bash implementation

--- include.assert.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-function assertOneArg {
-    if [ "$#" != 1 ]; then
-        echo "expected one argument in \"$(basename ${BASH_SOURCE[1]}):${BASH_LINENO[0]}:${FUNCNAME[1]}()\""
-        exit 1
-    fi
-}
