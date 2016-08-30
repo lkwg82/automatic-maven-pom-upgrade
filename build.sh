e16ad4976242525e6e864524e1459a5fe31c07f3
lars.gohlke <lars.gohlke@idealo.de>
2016-08-30 20:01:24 +0200

    adds build with commid id

--- build.sh
+++ build.sh
@@ -17,6 +17,12 @@ go test -v ./...
 #> logs/test.log && rm logs/test.log || bash -c 'cat logs/test.log && exit 1'
 #go fmt ./...
 
+# cross compiling
+# https://golang.org/doc/install/source#environment
+
 mkdir -p bin
 echo " building"
-go build -ldflags="-s -w" -o bin/upgrade main.go > logs/build.log && rm logs/build.log || bash -c 'cat logs/build.log && exit 1'
+env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o bin/upgrade main.go > logs/build.log && rm logs/build.log || bash -c 'cat logs/build.log && exit 1'
+
+mkdir -p dist
+cp bin/upgrade dist/upgrade-$(git rev-parse HEAD)-linux-amd64
