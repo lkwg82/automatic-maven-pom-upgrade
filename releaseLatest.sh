314b73ee34d1f16c7d3a598f34335a5c22574dce
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-09-02 01:39:43 +0200

    debug

--- releaseLatest.sh
+++ releaseLatest.sh
@@ -17,8 +17,6 @@ git config user.email "builds@travis-ci.com"
 git config user.name "Travis CI"
 git remote add origin2 git@github.com:${TRAVIS_REPO_SLUG}.git
 find .
-ssh-add travis
-
 git push --delete origin2 latest || echo "no latest tag"
 
 DESCRIPTION=$(echo -n "will_be_released_with_each_successful_commit__${TRAVIS_COMMIT} at ${TRAVIS_STACK_TIMESTAMP}" | sed -e 's# #_#g')
