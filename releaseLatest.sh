46c7d71006e0cfbac64fc550c681755a4ad59426
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-09-02 00:19:45 +0200

    debug travisci

--- releaseLatest.sh
+++ releaseLatest.sh
@@ -8,13 +8,15 @@ go get github.com/aktau/github-release
 github-release --help
 
 function tool() {
+ set +x
  local cmd=$1
  shift
+ set -x
  github-release ${cmd} --user lkwg82 --repo automatic-maven-pom-upgrade --tag "latest" $@
 }
 
-tool delete 2>/dev/null || echo "no latest release yet"
+tool delete || echo "no latest release yet"
 
-tool release --name "latest" --pre-release --description "will_be_released_with_each_successful_commit"
+tool release --pre-release --description "will_be_released_with_each_successful_commit"
 tool upload --name upgrade_linux_amd64 -f bin/upgrade_linux_amd64
 tool upload --name upgrade_darwin_amd64 -f bin/upgrade_darwin_amd64
