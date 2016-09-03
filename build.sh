ae62740a026d921f82721586ee296e0d29d1530d
Lars K.W. Gohlke <lkwg82@gmx.de>
2016-09-03 16:11:55 +0200

    make build script more convinient
    * adds syntax check before executing script
    * add convinient build selection when not in release mode
    * debug travis
    * debug travis
    
    * debug
    * fix unknown architecture on travis

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -e
 #set -x
 
-mkdir -p logs
+# syntax check
+bash -n $0
 
 if [ -z "$SKIP_TESTS" ]; then
     echo " run tests"
@@ -13,27 +14,67 @@ fi
 mkdir -p bin
 echo " building"
 
+determineCurrentOS() {
+    case $(uname | tr [:upper:] [:lower:]) in
+        "darwin")
+            export CURRENT_GOOS="darwin"
+        ;;
+        "linux")
+            export CURRENT_GOOS="linux"
+        ;;
+    esac
+}
+
+determineCurrentArch() {
+     set -x
+     local arch=$(uname -p)
+     set +x
+     case  ${arch} in
+        "x86_64")
+            export CURRENT_GOARCH="amd64"
+        ;;
+        "unknown")
+            echo "falling back to x86_64 architecture"
+            export CURRENT_GOARCH="amd64"
+        ;;
+    esac
+}
+
 build() {
+  export GOOS=$1
+  export GOARCH=$2
   go build -ldflags="-s -w" -o bin/upgrade_${GOOS}_${GOARCH} main.go
+  [ -n "$RELEASE" ] || ln -sf bin/upgrade_${GOOS}_${GOARCH} bin/upgrade
+  unset GOOS
+  unset GOARCH
+  _upx $1 $2
 }
 
 _upx() {
-  [ -n "$RELEASE" ] && upx --ultra-brute bin/upgrade_${GOOS}_${GOARCH} || echo -n ""
+  [ -n "$RELEASE" ] && upx --ultra-brute bin/upgrade_$1_$2 || echo -n ""
 }
-
-build
 
 # cross compiling
 # https://golang.org/doc/install/source#environment
-export GOOS=linux
-export GOARCH=amd64
-build
-_upx
+determineCurrentOS
+determineCurrentArch
 
-export GOOS=darwin
-export GOARCH=amd64
-build
-_upx
+if [ -n "$RELEASE" ]; then
+            build darwin amd64
+            build linux amd64
+else
+    if [ "$CURRENT_GOARCH" == "amd64" ]; then
+        if [ "$CURRENT_GOOS" == "darwin" ]; then
+            build darwin amd64
+        elif [ "$CURRENT_GOOS" == "linux" ]; then
+            build linux amd64
+        fi
+    else
+        echo "dont support current arch: $CURRENT_GOARCH"
+        exit 1
+    fi
+fi
 
-unset GOOS
-unset GOARCH
+
+
+
